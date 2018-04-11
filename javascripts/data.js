45fe11d7a07c541021362bb6939c0e7af422b705
Mehret Kulala <merutaye@gmail.com>
2018-04-10 21:24:34 -0500

    xhr function complete

---
+++ javascripts/data.js
@@ -0,0 +1,24 @@
+const loadDinos = require('./dinosaurs');
+
+let dinoArray = [];
+const whenDinosLoad = function () {
+  dinoArray = JSON.parse(this.responseText).dinosaurs;
+  console.log('dinoArray', dinoArray);
+
+};
+
+const badDinos = function () {
+  console.error('something broke');
+};
+
+const initializer = () => {
+  loadDinos(whenDinosLoad, badDinos);
+};
+
+const getDinos = () => {
+  return dinoArray;
+};
+module.exports = {
+  initializer,
+  getDinos,
+};
